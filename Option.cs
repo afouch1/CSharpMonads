c86797ae1df4606c91f777a1155fb54f5d72b099
afouch <afouch1@yahoo.com>
2020-04-26 20:17:36 -0400

    Added more Extension Methods

--- Option.cs
+++ Option.cs
@@ -4,7 +4,7 @@ namespace Monads
 {
 	public class Option<T>
 	{
-		public static implicit operator Option<T>(T obj) => 
+		public static implicit operator Option<T>(T obj) =>
 			obj is null ? Option.None<T>() : Option.Some(obj);
 
 		internal Option() { }
@@ -25,6 +25,79 @@ namespace Monads
 				Some<T> s => pred(s.Value),
 				_ => Option.None<TR>()
 			};
+		}
+
+		public Option<T> OnSomeTry(Action<T> Try, Action<Exception> Catch)
+		{
+			if (this is None<T>) return this;
+			try
+			{
+				var s = (Some<T>) this;
+				Try(s);
+			}
+			catch (Exception e)
+			{
+				Catch(e);
+			}
+
+			return this;
+		}
+		
+		public Option<TOutput> OnSomeTry<TOutput>(Func<T, TOutput> Try, Func<Exception, TOutput> Catch)
+		{
+			if (this is None<T>) return Option.None<TOutput>();
+			try
+			{
+				var s = (Some<T>) this;
+				return Try(s);
+			}
+			catch (Exception e)
+			{
+				return Catch(e);
+			}
+		}
+		
+		public Option<TOutput> OnSomeTry<TOutput>(Func<T, TOutput> Try, Action<Exception> Catch)
+		{
+			if (this is None<T>) return Option.None<TOutput>();
+			try
+			{
+				var s = (Some<T>) this;
+				return Try(s);
+			}
+			catch (Exception e)
+			{
+				Catch(e);
+				return Option.None<TOutput>();
+			}
+		}
+
+		public Option<T> OnSomeTry<TException>(Action<T> Try, Action<TException> Catch) where TException: Exception
+		{
+			if (this is None<T>) return this;
+			try
+			{
+				var s = (Some<T>) this;
+				Try(s);
+			}
+			catch (TException e)
+			{
+				Catch(e);
+			}
+
+			return this;
+		}
+
+		public Option<T> OnSome(Action<T> onsome)
+		{
+			if (this is Some<T> s) onsome(s);
+			return this;
+		}
+
+		public Option<T> OnNone(Action onnone)
+		{
+			if (this is None<T>) onnone();
+			return this;
 		}
 
 		public T Expect<TException>(string message) where TException: Exception
@@ -48,7 +121,7 @@ namespace Monads
 		
 		public static implicit operator T(Some<T> s) => s.Value;
 	}
-
+	
 	public sealed class None<T> : Option<T>
 	{
 		internal None() { }
--- Result.cs
+++ Result.cs
@@ -1,5 +1,9 @@
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
 using System.Security.Principal;
 
 namespace Monads
@@ -11,7 +15,9 @@ namespace Monads
 		public static implicit operator Result<TOk, TError>(TOk obj) => Result.Ok<TOk, TError>(obj);
 
 		public static implicit operator Result<TOk, TError>(TError obj) => Result.Error<TOk, TError>(obj);
-
+		
+		#region Higher Order Functions
+		
 		public Result<TOutput, TError> Map<TOutput>(Func<TOk, TOutput> pred)
 		{
 			return this switch
@@ -39,6 +45,18 @@ namespace Monads
 			};
 		}
 
+		public Result<TOk, TError> OnSuccess(Action<TOk> onsuccess)
+		{
+			if (this is Ok<TOk, TError> ok) onsuccess(ok);
+			return this;
+		}
+
+		public Result<TOk, TError> OnError(Action<TError> onerror)
+		{
+			if (this is Error<TOk, TError> err) onerror(err);
+			return this;
+		}
+
 		public TOk Expect<TException>(string errorMessage) where TException : Exception
 		{
 			return this switch
@@ -47,6 +65,120 @@ namespace Monads
 				_ => throw (Exception) Activator.CreateInstance(typeof(TException), errorMessage)
 			};
 		}
+		
+		public TOutput Expect<TException, TOutput>(Func<TOk, TOutput> mapper, string errorMessage) where TException : Exception
+		{
+			return this switch
+			{
+				Ok<TOk, TError> ok => mapper(ok),
+				_ => throw (Exception) Activator.CreateInstance(typeof(TException), errorMessage)
+			};
+		}
+		
+		public TOk Expect<TException>(Func<TException> exception) where TException : Exception
+		{
+			return this switch
+			{
+				Ok<TOk, TError> ok => ok,
+				_ => throw exception()
+			};
+		}
+		
+		public TOutput Expect<TException, TOutput>(Func<TOk, TOutput> mapper, Func<TException> exception) where TException : Exception
+		{
+			return this switch
+			{
+				Ok<TOk, TError> ok => mapper(ok),
+				_ => throw exception()
+			};
+		}
+
+		public Result<TOkOutput, TError> MapTry<TOkOutput>(Func<TOk, TOkOutput> Try, Func<Exception, TError> Catch)
+		{
+			if (this is Error<TOk, TError> err) return Result.Error<TOkOutput, TError>(err.ErrorValue);
+			try
+			{
+				var ok = (Ok<TOk, TError>) this;
+				return Try(ok);
+			}
+			catch (Exception e)
+			{
+				return Catch(e);
+			}
+		}
+
+		public Result<TOkOutput, TError> BindTry<TOkOutput>(Func<TOk, Result<TOkOutput, TError>> Try, 
+			Func<Exception, Result<TOkOutput, TError>> Catch)
+		{
+			if (this is Error<TOk, TError> err) return Result.Error<TOkOutput, TError>(err.ErrorValue);
+			try
+			{
+				var ok = (Ok<TOk, TError>) this;
+				return Try(ok);
+			}
+			catch (Exception e)
+			{
+				return Catch(e);
+			}
+		}
+		
+		public Result<TOkOutput, TError> MapTry<TOkOutput, TException>(Func<TOk, TOkOutput> Try, Func<TException, TError> Catch)
+			where TException: Exception
+		{
+			if (this is Error<TOk, TError> err) return Result.Error<TOkOutput, TError>(err.ErrorValue);
+			try
+			{
+				var ok = (Ok<TOk, TError>) this;
+				return Try(ok);
+			}
+			catch (TException e)
+			{
+				return Catch(e);
+			}
+		}
+
+		public Result<TOkOutput, TError> BindTry<TOkOutput, TException>(Func<TOk, Result<TOkOutput, TError>> Try, 
+			Func<Exception, Result<TOkOutput, TError>> Catch)
+			where TException: Exception
+		{
+			if (this is Error<TOk, TError> err) return Result.Error<TOkOutput, TError>(err.ErrorValue);
+			try
+			{
+				var ok = (Ok<TOk, TError>) this;
+				return Try(ok);
+			}
+			catch (TException e)
+			{
+				return Catch(e);
+			}
+		}
+
+		public void Try<TException>(Action<Result<TOk, TError>> Try, Action<TException> Catch) where TException: Exception
+		{
+			try
+			{
+				Try(this);
+			}
+			catch (TException e)
+			{
+				Catch(e);
+			}
+		}
+
+		public TOutput Try<TException, TOutput>(Func<Result<TOk, TError>, TOutput> Try, Func<TException, TOutput> Catch) where TException : 
+		Exception
+		{
+			try
+			{
+				return Try(this);
+			}
+			catch (TException e)
+			{
+				return Catch(e);
+			}
+		}
+
+		#endregion
 	}
 
 	public sealed class Ok<TOk, TError> : Result<TOk, TError>
@@ -82,12 +214,5 @@ namespace Monads
 		public static Result<TOk, TError> Ok<TOk, TError>(TOk obj) => new Ok<TOk, TError>(obj);
 		
 		public static Result<TOk, TError> Error<TOk, TError>(TError err) => new Error<TOk, TError>(err);
-
-        public static T Expect<T, TException>(this Result<T, string> res) where TException : Exception
-        {
-            if (res is Error<T, string> err)
-                throw (Exception) Activator.CreateInstance(typeof(TException), err);
-            else return (Ok<T, string>) res;
-        }
 	}
 }
